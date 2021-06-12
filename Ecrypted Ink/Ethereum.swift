3d21ed7eca115efd35776e0ca884636b800ef706
Ivan Grachyov <mail@grachyov.com>
2021-06-12 14:30:58 +0300

    Add stubs

---
+++ Ecrypted Ink/Ethereum.swift
@@ -0,0 +1,35 @@
+// Copyright © 2021 Encrypted Ink. All rights reserved.
+
+import Foundation
+
+struct Transaction {
+    
+}
+
+struct Account {
+    
+}
+
+struct Ethereum {
+    
+    static func sign(message: String, account: Account) {
+        
+    }
+    
+    static func signPersonal(message: String, account: Account) {
+        
+    }
+    
+    static func sign(typedData: String, account: Account) {
+        
+    }
+    
+    static func sign(transaction: Transaction, account: Account) {
+        
+    }
+    
+    static func send(transaction: Transaction, account: Account) {
+        
+    }
+    
+}
