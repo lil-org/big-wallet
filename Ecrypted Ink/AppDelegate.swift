785da935d9703b2a38d452489605af9c7b548bf8
Ivan Grachyov <mail@grachyov.com>
2021-06-12 13:32:27 +0300

    Initial Commit

---
+++ Ecrypted Ink/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ecrypted Ink
+//
+//  Created by Ivan Grachev on 12.06.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ecrypted Ink/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ecrypted Ink
+//
+//  Created by Ivan Grachev on 12.06.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
