374576cd90f5ff1ab0d5b5143f99045323e14b8d
Ivan Grachyov <mail@grachyov.com>
2021-06-13 07:20:23 +0300

    Display error message on error screen

--- Encrypted Ink/Screens/ErrorViewController.swift
+++ Encrypted Ink/Screens/ErrorViewController.swift
@@ -13,10 +13,11 @@ class ErrorViewController: NSViewController {
     private var message = ""
     
     @IBOutlet weak var titleLabel: NSTextField!
+    @IBOutlet weak var messageLabel: NSTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: show message text
+        messageLabel.stringValue = message
     }
 
     @IBAction func actionButtonTapped(_ sender: Any) {
