8430084163f0cb468850cc21b8678348b50aa73f
Ivan Grachyov <mail@grachyov.com>
2021-06-19 14:07:57 +0300

    Implement quitting from status bar menu

--- Encrypted Ink/Agent.swift
+++ Encrypted Ink/Agent.swift
@@ -4,12 +4,12 @@ import Cocoa
 import WalletConnect
 import LocalAuthentication
 
-class Agent {
+class Agent: NSObject {
 
     static let shared = Agent()
     private lazy var statusImage = NSImage(named: "Status")
     
-    private init() {}
+    private override init() { super.init() }
     private var statusBarItem: NSStatusItem!
     private var hasPassword = Keychain.password != nil
     
@@ -72,15 +72,6 @@ class Agent {
         windowController.contentViewController = ErrorViewController.withMessage(message)
     }
     
-    func setupStatusBarItem() {
-        let statusBar = NSStatusBar.system
-        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
-        statusBarItem.button?.image = statusImage
-        statusBarItem.button?.target = self
-        statusBarItem.button?.action = #selector(statusBarButtonClicked(sender:))
-        statusBarItem.button?.sendAction(on: [.leftMouseUp])
-    }
-    
     func processInputLink(_ link: String) {
         let session = sessionWithLink(link)
         showInitialScreen(onAppStart: false, wcSession: session)
@@ -89,6 +80,38 @@ class Agent {
     func getAccountSelectionCompletionIfShouldSelect() -> ((Account) -> Void)? {
         let session = getSessionFromPasteboard()
         return onSelectedAccount(session: session)
+    }
+    
+    lazy private var statusBarMenu: NSMenu = {
+        let menu = NSMenu(title: "Encrypted Ink")
+        let quitItem = NSMenuItem(title: "Quit", action: #selector(didSelectQuitMenuItem), keyEquivalent: "q")
+        quitItem.target = self
+        menu.delegate = self
+        menu.addItem(quitItem)
+        return menu
+    }()
+    
+    @objc private func didSelectQuitMenuItem() {
+        // TODO: show alert
+        NSApp.terminate(nil)
+    }
+    
+    func setupStatusBarItem() {
+        let statusBar = NSStatusBar.system
+        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
+        statusBarItem.button?.image = statusImage
+        statusBarItem.button?.target = self
+        statusBarItem.button?.action = #selector(statusBarButtonClicked(sender:))
+        statusBarItem.button?.sendAction(on: [.leftMouseUp, .rightMouseUp])
+    }
+    
+    @objc private func statusBarButtonClicked(sender: NSStatusBarButton) {
+        if let event = NSApp.currentEvent, event.type == .rightMouseUp {
+            statusBarItem.menu = statusBarMenu
+            statusBarItem.button?.performClick(nil)
+        } else {
+            checkPasteboardAndOpen(onAppStart: false)
+        }
     }
     
     private func onSelectedAccount(session: WCSession?) -> ((Account) -> Void)? {
@@ -115,10 +138,6 @@ class Agent {
     
     private func sessionWithLink(_ link: String) -> WCSession? {
         return WalletConnect.shared.sessionWithLink(link)
-    }
-    
-    @objc private func statusBarButtonClicked(sender: NSStatusBarButton) {
-        checkPasteboardAndOpen(onAppStart: false)
     }
     
     func proceedAfterAuthentication(reason: String, completion: @escaping (Bool) -> Void) {
@@ -150,6 +169,14 @@ class Agent {
         
         let windowController = Window.showNew()
         windowController.contentViewController = WaitingViewController.withReason("Connecting")
+    }
+    
+}
+
+extension Agent: NSMenuDelegate {
+    
+    func menuDidClose(_ menu: NSMenu) {
+        statusBarItem.menu = nil
     }
     
 }
