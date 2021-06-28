69683ee06cf6cbde66ed0092a70a8f19d1b50803
Ivan Grachyov <mail@grachyov.com>
2021-06-28 17:22:41 +0300

    Implement gas service

--- Encrypted Ink/Services/GasService.swift
+++ Encrypted Ink/Services/GasService.swift
@@ -2,22 +2,49 @@
 
 import Foundation
 
-struct GasService {
+class GasService {
+    
+    struct Message: Codable {
+        let data: Info
+    }
     
     struct Info: Codable {
-        let standard: Int
-        let slow: Int
-        let fast: Int
-        let rapid: Int
+        let standard: UInt
+        let slow: UInt
+        let fast: UInt
+        let rapid: UInt
     }
     
     static let shared = GasService()
-    private init() {}
+    
+    private let webSocketTask: URLSessionWebSocketTask
+    private let jsonDecoder = JSONDecoder()
+    
+    private init() {
+        let url = URL(string: "wss://www.gasnow.org/ws/gasprice")!
+        let urlSession = URLSession(configuration: .default)
+        webSocketTask = urlSession.webSocketTask(with: url)
+    }
     
     var currentInfo: Info?
     
     func start() {
-        
+        webSocketTask.resume()
+        getMessage()
+    }
+    
+    private func getMessage() {
+        webSocketTask.receive { [weak self] result in
+            if case let .success(message) = result,
+               case let .string(text) = message,
+               let data = text.data(using: .utf8),
+               let info = try? self?.jsonDecoder.decode(Message.self, from: data).data {
+                DispatchQueue.main.async {
+                    self?.currentInfo = info
+                }
+            }
+            self?.getMessage()
+        }
     }
     
 }
