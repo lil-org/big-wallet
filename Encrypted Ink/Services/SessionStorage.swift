4db073ecbfda45be44a78e250329c326a6a7a4d8
Ivan Grachev <ivan@ivan.lol>
2021-07-10 22:41:53 +0300

    Add SessionStorage stub

---
+++ Encrypted Ink/Services/SessionStorage.swift
@@ -0,0 +1,11 @@
+// Copyright © 2021 Encrypted Ink. All rights reserved.
+
+import Foundation
+
+class SessionStorage {
+    
+    static let shared = SessionStorage()
+    
+    private init() {}
+    
+}
