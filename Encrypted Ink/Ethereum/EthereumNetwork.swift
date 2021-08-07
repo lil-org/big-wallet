361ebc96e87cfeb90129ea1108982c994d7240ac
Ivan Grachyov <ivan@ivan.lol>
2021-08-07 19:51:33 +0300

    Fix network type

--- Encrypted Ink/Ethereum/EthereumNetwork.swift
+++ Encrypted Ink/Ethereum/EthereumNetwork.swift
@@ -5,7 +5,7 @@ import Web3Swift
 
 final class EthereumNetwork: Network {
     
-    static let allByChain: [EthereumChain: EthereumNetwork] = [
+    static let allByChain: [EthereumChain: Network] = [
         .main: EthereumNetwork(url: "https://eth-mainnet.alchemyapi.io/v2/" + Secrets.alchemy),
         .polygon: EthereumNetwork(url: "https://polygon-mainnet.g.alchemy.com/v2/" + Secrets.alchemy),
         .arbitrum: EthereumNetwork(url: "https://arb-mainnet.g.alchemy.com/v2/" + Secrets.alchemy),
