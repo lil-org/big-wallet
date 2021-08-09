2611f9b92ca49e37a92fc7293bed395ac08028ff
Ivan Grachyov <ivan@ivan.lol>
2021-08-10 02:53:13 +0300

    Add optimism to Safari provider

--- Encrypted-Ink-Safari/Resources/src/provider.js
+++ Encrypted-Ink-Safari/Resources/src/provider.js
@@ -5,7 +5,8 @@ const wcProvider = new WalletConnectProvider({
   rpc: {
     1: "https://mainnet.infura.io/v3/8b014620351a4cbe814220743619df5b",
     56: "https://bsc-dataseed.binance.org",
-    2020: "https://api.roninchain.com/rpc/"
+    2020: "https://api.roninchain.com/rpc/",
+    10: "https://mainnet.optimism.io"
   }
 });
 
