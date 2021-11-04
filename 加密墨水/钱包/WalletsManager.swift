a29a6ad4dabc01c27041cfb6bdc3fe7ccd331c21
GUANLONGH <87150503+GUANLONGH@users.noreply.github.com>
2021-11-04 23:45:54 +0200

    Update and rename Encrypted Ink/Wallets/WalletsManager.swift to 加密墨水/钱包/WalletsManager.swift

--- 加密墨水/钱包/WalletsManager.swift
+++ 加密墨水/钱包/WalletsManager.swift
@@ -1,4 +1,4 @@
-// Copyright © 2021 Encrypted Ink. All rights reserved.
+// Copyright © 2021 guanlong huang. All rights reserved.
 // Rewrite of KeyStore.swift from Trust Wallet Core.
 
 import Foundation
