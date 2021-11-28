266c5867e4727765af11c54d2a1d3b13bc2039b4
Ivan Grachev <ivan@ivan.lol>
2021-11-28 15:19:04 +0300

    Add TODOs

--- Safari/Resources/background.js
+++ Safari/Resources/background.js
@@ -10,6 +10,8 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
 browser.browserAction.onClicked.addListener(function(tab) {
     const id = new Date().getTime() + Math.floor(Math.random() * 1000);
     const request = {id: id, name: "switchAccount", object: {}, address: ""};
+    // TODO: pass current network id
+    // TODO: pass favicon
     // TODO: pass host here as well
     browser.runtime.sendNativeMessage("ink.encrypted.macos", request, function(response) {
         browser.tabs.sendMessage(tab.id, response);
