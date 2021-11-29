2e53229507363db455fe6844fc85cb8d068bcb1e
Ivan Grachyov <ivan@ivan.lol>
2021-11-29 18:24:43 +0300

    Switch to Infura

--- Encrypted Ink/Ethereum/EthereumChain.swift
+++ Encrypted Ink/Ethereum/EthereumChain.swift
@@ -82,23 +82,23 @@ enum EthereumChain: Int {
     
     var nodeURLString: String {
         switch self {
-        case .ethereum: return "https://mainnet.infura.io/v3/3f99b6096fda424bbb26e17866dcddfc"
-        case .arbitrum: return "https://arb-mainnet.g.alchemy.com/v2/" + Secrets.alchemy
-        case .optimism: return "https://mainnet.optimism.io"
-        case .polygon: return "https://polygon-mainnet.g.alchemy.com/v2/" + Secrets.alchemy
+        case .ethereum: return "https://mainnet.infura.io/v3/" + Secrets.infura
+        case .arbitrum: return "https://arbitrum-mainnet.infura.io/v3/" + Secrets.infura
+        case .optimism: return "https://optimism-mainnet.infura.io/v3/" + Secrets.infura
+        case .polygon: return "https://polygon-mainnet.infura.io/v3/" + Secrets.infura
         case .binance: return "https://bsc-dataseed.binance.org/"
         case .avalanche: return "https://api.avax.network/ext/bc/C/rpc"
         case .xDai: return "https://rpc.xdaichain.com/"
         case .arbitrumRinkeby: return "https://rinkeby.arbitrum.io/rpc"
         case .arbitrumKovan: return "https://kovan5.arbitrum.io/rpc"
         case .optimisticKovan: return "https://kovan.optimism.io"
-        case .polygonMumbai: return "https://polygon-mumbai.g.alchemy.com/v2/" + Secrets.alchemy
-        case .ethereumRopsten: return "https://ropsten.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
-        case .ethereumKovan: return "https://kovan.infura.io/v3/3f99b6096fda424bbb26e17866dcddfc"
-        case .ethereumRinkeby: return "https://eth-rinkeby.alchemyapi.io/v2/" + Secrets.alchemy
-        case .ethereumGoerli: return "https://goerli.infura.io/v3/3f99b6096fda424bbb26e17866dcddfc"
+        case .ethereumRinkeby: return "https://rinkeby.infura.io/v3/" + Secrets.infura
+        case .ethereumRopsten: return "https://ropsten.infura.io/v3/" + Secrets.infura
+        case .ethereumKovan: return "https://kovan.infura.io/v3/" + Secrets.infura
+        case .ethereumGoerli: return "https://goerli.infura.io/v3/" + Secrets.infura
         case .binanceTestnet: return "https://data-seed-prebsc-1-s1.binance.org:8545/"
         case .avalancheFuji: return "https://api.avax-test.network/ext/bc/C/rpc"
+        case .polygonMumbai: return "https://polygon-mumbai.infura.io/v3/" + Secrets.infura
         }
     }
     
