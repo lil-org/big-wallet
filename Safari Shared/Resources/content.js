11a4ec256e03de09869f37d79b4710660247b156
Ivan Grachev <ivan@ivan.lol>
2022-01-24 18:07:29 +0300

    Reload page on interactive document state

--- Safari Shared/Resources/content.js
+++ Safari Shared/Resources/content.js
@@ -4,7 +4,7 @@ if (window.location.href.startsWith("https://tokenary.io/blank")) {
     browser.runtime.sendMessage({ subject: "wakeUp" });
 }
 
-if (document.readyState === "complete") {
+if (document.readyState != "loading") {
     window.location.reload();
 }
 
