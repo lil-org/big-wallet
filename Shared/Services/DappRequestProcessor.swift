d6d04a07648bf9ce84780af88e9cf914a61b6bf9
Ivan Grachev <ivan@ivan.lol>
2022-02-14 21:54:38 +0300

    Move DappRequestAction into a separate file

--- Shared/Services/DappRequestProcessor.swift
+++ Shared/Services/DappRequestProcessor.swift
@@ -183,34 +183,3 @@ struct DappRequestProcessor {
     }
     
 }
-
-enum DappRequestAction {
-    case none
-    case justShowApp
-    case selectAccount(SelectAccountAction)
-    case approveMessage(SignMessageAction)
-    case approveTransaction(SendTransactionAction)
-}
-
-struct SelectAccountAction {
-    let provider: Web3Provider
-    let completion: (EthereumChain?, TokenaryWallet?) -> Void
-}
-
-struct SignMessageAction {
-    let provider: Web3Provider
-    let subject: ApprovalSubject
-    let address: String
-    let meta: String
-    let peerMeta: PeerMeta
-    let completion: (Bool) -> Void
-}
-
-struct SendTransactionAction {
-    let provider: Web3Provider
-    let transaction: Transaction
-    let chain: EthereumChain
-    let address: String
-    let peerMeta: PeerMeta
-    let completion: (Transaction?) -> Void
-}
