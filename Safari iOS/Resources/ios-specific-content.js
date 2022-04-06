1c936053e1b7742f53fec14dedadd8e664fb8cab
Ivan Grachev <ivan@ivan.lol>
2022-04-06 23:56:13 +0300

    Fix launching iOS app on extension's request

--- Safari iOS/Resources/ios-specific-content.js
+++ Safari iOS/Resources/ios-specific-content.js
@@ -2,6 +2,6 @@ function platformSpecificProcessMessage(message) {
     if (message.provider == "ethereum" && (message.name == "switchEthereumChain" || message.name == "addEthereumChain")) {
         return;
     } else {
-        window.location.href = "tokenary://" + JSON.stringify(message);
+        window.location.href = "tokenary://" + encodeURIComponent(JSON.stringify(message));
     }
 }
