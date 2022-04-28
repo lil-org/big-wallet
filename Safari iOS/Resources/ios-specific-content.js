a655061506d0575eb969cee68a0730b6027e8b9b
Ivan Grachev <ivan@ivan.lol>
2022-04-28 23:47:14 +0300

    Deliver responses on iOS without using a blank page

--- Safari iOS/Resources/ios-specific-content.js
+++ Safari iOS/Resources/ios-specific-content.js
@@ -1,3 +1,18 @@
+// Copyright © 2022 Tokenary. All rights reserved.
+
+function didChangeVisibility() {
+    if (document.visibilityState === 'visible' && pendingRequestsIds.size != 0) {
+        pendingRequestsIds.forEach(id => {
+            const request = {id: id, subject: "getResponse"};
+            browser.runtime.sendMessage(request).then((response) => {
+                sendToInpage(response, id);
+            });
+        });
+    }
+}
+
+document.addEventListener('visibilitychange', didChangeVisibility);
+
 function platformSpecificProcessMessage(message) {
     if (message.provider == "ethereum" && (message.name == "switchEthereumChain" || message.name == "addEthereumChain")) {
         return;
