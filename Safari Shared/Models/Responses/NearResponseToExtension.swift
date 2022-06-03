e99b48d42485bf71204e48a321a4fe28267640df
Ivan Grachev <ivan@ivan.lol>
2022-06-03 19:54:24 +0300

    Update Near response to extension

--- Safari Shared/Models/Responses/NearResponseToExtension.swift
+++ Safari Shared/Models/Responses/NearResponseToExtension.swift
@@ -7,9 +7,17 @@ extension ResponseToExtension {
     struct Near: Codable {
         
         let account: String?
+        let response: String?
         
-        init(account: String? = nil) {
+        init(account: String? = nil, response: [[String: Any]]? = nil) {
             self.account = account
+            if let response = response,
+               let responseData = try? JSONSerialization.data(withJSONObject: response, options: .fragmentsAllowed),
+               let jsonString = String(data: responseData, encoding: .utf8) {
+                self.response = jsonString
+            } else {
+                self.response = nil
+            }
         }
         
     }
