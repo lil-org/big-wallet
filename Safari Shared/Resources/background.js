739119acf517c31927db752791e6b1a42bb16437
Ivan Grachev <ivan@ivan.lol>
2022-07-11 19:01:19 +0300

    Read and write the latest configuration in the background script

--- Safari Shared/Resources/background.js
+++ Safari Shared/Resources/background.js
@@ -6,14 +6,40 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
             sendResponse(response);
             browser.tabs.update(sender.tab.id, { active: true });
             didCompleteRequest(request.message.id);
+            storeConfigurationIfNeeded(request.host, response);
         });
     } else if (request.subject === "getResponse") {
         browser.runtime.sendNativeMessage("mac.tokenary.io", request, function(response) {
             sendResponse(response);
+            storeConfigurationIfNeeded(request.host, response);
         });
+    } else if (request.subject === "getLatestConfiguration") {
+        getLatestConfiguration(request.host, sendResponse);
     }
     return true;
 });
+
+function getLatestConfiguration(host, sendResponse) {
+    const storageItem = browser.storage.local.get(host);
+    storageItem.then((storage) => {
+        var response = {};
+        
+        const latest = storage[host];
+        if (typeof latest !== "undefined") {
+            response = latest;
+        }
+        
+        response.name = "didLoadLatestConfiguration";
+        sendResponse(response);
+    });
+}
+
+function storeConfigurationIfNeeded(host, response) {
+    if (host.length > 0 && "configurationToStore" in response) {
+        const latest = response.configurationToStore;
+        browser.storage.local.set( {[host]: latest});
+    }
+}
 
 browser.browserAction.onClicked.addListener(function(tab) {
     const message = {didTapExtensionButton: true};
