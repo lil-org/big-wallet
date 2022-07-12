1250e93984659a7fc56ce11ab1a70162f9cafe79
Ivan Grachev <ivan@ivan.lol>
2022-07-12 14:58:14 +0300

    Minimise browser.storage access when reading latest configurations

--- Safari Shared/Resources/background.js
+++ Safari Shared/Resources/background.js
@@ -19,25 +19,60 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     return true;
 });
 
+var latestConfigurations = {};
+var didReadLatestConfigurations = false;
+
+function respondWithLatestConfiguration(host, sendResponse) {
+    var response = {};
+    
+    const latest = latestConfigurations[host];
+    if (typeof latest !== "undefined") {
+        response = latest;
+    }
+    
+    response.name = "didLoadLatestConfiguration";
+    sendResponse(response);
+}
+
+function storeLatestConfiguration(host, configuration) {
+    // TODO: merge with current value if there is a different provider
+    
+    latestConfigurations[host] = configuration;
+    browser.storage.local.set( {[host]: configuration});
+    
+    console.log("did store new latest configuration", host, configuration);
+    console.log(latestConfigurations);
+}
+
 function getLatestConfiguration(host, sendResponse) {
-    const storageItem = browser.storage.local.get(host);
+    if (didReadLatestConfigurations) {
+        respondWithLatestConfiguration(host, sendResponse);
+        return;
+    }
+    
+    const storageItem = browser.storage.local.get();
     storageItem.then((storage) => {
-        var response = {};
-        
-        const latest = storage[host];
-        if (typeof latest !== "undefined") {
-            response = latest;
-        }
-        
-        response.name = "didLoadLatestConfiguration";
-        sendResponse(response);
+        latestConfigurations = storage;
+        didReadLatestConfigurations = true;
+        respondWithLatestConfiguration(host, sendResponse);
     });
 }
 
 function storeConfigurationIfNeeded(host, response) {
     if (host.length > 0 && "configurationToStore" in response) {
-        const latest = response.configurationToStore;
-        browser.storage.local.set( {[host]: latest});
+        const configuration = response.configurationToStore;
+        
+        if (didReadLatestConfigurations) {
+            storeLatestConfiguration(host, configuration);
+            return;
+        }
+        
+        const storageItem = browser.storage.local.get();
+        storageItem.then((storage) => {
+            latestConfigurations = storage;
+            didReadLatestConfigurations = true;
+            storeLatestConfiguration(host, configuration);
+        });
     }
 }
 
