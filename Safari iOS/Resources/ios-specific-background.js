3203f937401b7f539e82f33f9ab68d29da1cb20b
Ivan Grachev <ivan@ivan.lol>
2022-07-14 17:43:57 +0300

    Do not jump tabs on macOS

--- Safari iOS/Resources/ios-specific-background.js
+++ Safari iOS/Resources/ios-specific-background.js
@@ -1,6 +1,7 @@
 // Copyright © 2022 Tokenary. All rights reserved.
 
-function didCompleteRequest(id) {
+function didCompleteRequest(id, tabId) {
+    browser.tabs.update(tabId, { active: true });
     const request = {id: id, subject: "didCompleteRequest"};
     browser.runtime.sendNativeMessage("mac.tokenary.io", request);
 }
