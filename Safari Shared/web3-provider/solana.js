cff5fe2e8ca969de00a8b6697cca3e86307a4621
Ivan Grachev <ivan@ivan.lol>
2022-07-14 23:11:02 +0300

    Respect onlyIfTrusted parameter on Solana connect request

--- Safari Shared/web3-provider/solana.js
+++ Safari Shared/web3-provider/solana.js
@@ -59,8 +59,12 @@ class TokenarySolana extends EventEmitter {
         this.pendingPayloads = [];
     }
 
-    connect() {
-        return this.request({method: "connect"});
+    connect(params) {
+        var payload = {method: "connect"};
+        if (typeof params !== "undefined") {
+            payload.params = params;
+        }
+        return this.request(payload);
     }
 
     disconnect() {
@@ -143,7 +147,12 @@ class TokenarySolana extends EventEmitter {
         switch (payload.method) {
             case "connect":
                 if (!this.publicKey) {
-                    return this.postMessage("connect", payload.id, {});
+                    if ("params" in payload && "onlyIfTrusted" in payload.params && payload.params.onlyIfTrusted) {
+                        this.sendError(payload.id, "Click a button to connect");
+                        return;
+                    } else {
+                        return this.postMessage("connect", payload.id, {});
+                    }
                 } else {
                     this.isConnected = true;
                     this.emitConnect(this.publicKey);
