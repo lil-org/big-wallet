df03689a4ab348ea74eecb1039a8dbcb2ad181c1
ivan grachev <ivan@ivan.lol>
2023-10-23 18:13:51 +0300

    jump to the app from the ios popup

--- Safari iOS/Resources/popup.js
+++ Safari iOS/Resources/popup.js
@@ -1,13 +1,18 @@
 // Copyright © 2023 Tokenary. All rights reserved.
 
-document.getElementById('tokenary-button').addEventListener('click', () => {
+const button = document.getElementById('tokenary-button');
+
+button.addEventListener('click', () => {
+    const message = browser.extension.getBackgroundPage().sharedData;
+    const query = encodeURIComponent(JSON.stringify(message)) + '";';
     browser.tabs.executeScript({
-      code: 'window.location.href = "https://tokenary.io/extension";'
+      code: 'window.location.href = "https://tokenary.io/extension?query=' + query
     });
-    
-    // TODO: how to get a request body here?
     
     setTimeout( function() {
         window.close();
     }, 200);
 });
+
+// TODO: set title depending on a request
+button.innerText = "proceed in tokenary";
