a20210472f9fcf96236e04f7adcfbd65db6f7482
ivan grachev <ivan@ivan.lol>
2023-11-02 14:40:38 +0300

    clean up bundled nodes

--- tools/bundled/BundledNodes.swift
+++ tools/bundled/BundledNodes.swift
@@ -3,69 +3,69 @@ import Foundation
 struct BundledNodes {
     
     static let dict: [Int: String] = [
-        1: "mainnet.infura.io/v3/", // ⭐️ Ethereum Mainnet
-        5: "goerli.infura.io/v3/", // ⭐️ Goerli
-        10: "optimism-mainnet.infura.io/v3/", // ⭐️ OP Mainnet
-        56: "bsc-dataseed1.bnbchain.org", // ⭐️ BNB Smart Chain Mainnet
-        66: "exchainrpc.okex.org", // OKXChain Mainnet 
-        69: "kovan.optimism.io/", // ⭐️ Optimism Kovan
-        77: "sokol.poa.network", // POA Network Sokol
-        97: "data-seed-prebsc-1-s1.bnbchain.org:8545", // ⭐️ BNB Smart Chain Testnet
-        99: "core.poa.network", // POA Network Core
-        100: "rpc.gnosischain.com", // ⭐️ Gnosis
-        122: "rpc.fuse.io", // 🙄 Fuse Mainnet
-        128: "http-mainnet.hecochain.com", // Huobi ECO Chain Mainnet
-        137: "polygon-mainnet.infura.io/v3/", // ⭐️ Polygon Mainnet
-        200: "arbitrum.xdaichain.com/", // Arbitrum on xDai
-        204: "opbnb-mainnet-rpc.bnbchain.org", // opBNB Mainnet
-        250: "rpc.ftm.tools", // ⭐️ Fantom Opera
-        280: "testnet.era.zksync.dev", // zkSync Era Testnet
-        314: "api.node.glif.io/", // Filecoin - Mainnet
-        324: "mainnet.era.zksync.io", // zkSync Era Mainnet
-        420: "goerli.optimism.io", // Optimism Goerli Testnet 
-        424: "rpc.publicgoods.network", // PGN (Public Goods Network)
-        1001: "api.baobab.klaytn.net:8651", // Klaytn Testnet Baobab
-        1088: "andromeda.metis.io/?owner=1088", // 🙄 Metis Andromeda Mainnet
-        1101: "zkevm-rpc.com", // Polygon zkEVM
-        1442: "rpc.public.zkevm-test.net", // Polygon zkEVM Testnet
-        2221: "evm.testnet.kava.io", // 🙄 Kava Testnet
-        2222: "evm.kava.io", // 🙄 Kava
-        4002: "rpc.testnet.fantom.network", // ⭐️ Fantom Testnet
-        4337: "subnets.avax.network/beam/mainnet/rpc", // Beam
-        5000: "rpc.mantle.xyz", // 🙄 Mantle
-        5001: "rpc.testnet.mantle.xyz", // Mantle Testnet
-        5611: "opbnb-testnet-rpc.bnbchain.org", // opBNB Testnet
-        7700: "canto.slingshot.finance", // Canto
-        8217: "1rpc.io/klay", // ⭐️ Klaytn Mainnet Cypress
-        8453: "mainnet.base.org/", // ⭐️ Base
-        10200: "rpc.chiadochain.net", // Gnosis Chiado Testnet
-        42161: "arbitrum-mainnet.infura.io/v3/", // ⭐️ Arbitrum One
-        42170: "nova.arbitrum.io/rpc", // ⭐️ Arbitrum Nova
-        42220: "forno.celo.org", // ⭐️ Celo Mainnet
-        43113: "api.avax-test.network/ext/bc/C/rpc", // ⭐️ Avalanche Fuji Testnet
-        43114: "api.avax.network/ext/bc/C/rpc", // ⭐️ Avalanche C-Chain
-        44787: "alfajores-forno.celo-testnet.org", // Celo Alfajores Testnet
-        58008: "sepolia.publicgoods.network", // Sepolia PGN (Public Goods Network) 
-        59140: "rpc.goerli.linea.build", // 🙄 Linea Testnet
-        59144: "rpc.linea.build", // 🙄 Linea
-        64240: "rpcapi.sonic.fantom.network/", // ⭐️ Fantom Sonic Open
-        80001: "rpc-mumbai.maticvigil.com", // ⭐️ Mumbai
-        84531: "goerli.base.org", // Base Goerli Testnet 
-        84532: "sepolia.base.org", // Base Sepolia Testnet
-        421613: "goerli-rollup.arbitrum.io/rpc", // Arbitrum Goerli
-        421614: "sepolia-rollup.arbitrum.io/rpc", // Arbitrum Sepolia
-        534351: "sepolia-rpc.scroll.io", // ⭐️ Scroll Sepolia Testnet
-        534352: "rpc.scroll.io", // ⭐️ Scroll
-        7777777: "rpc.zora.energy/", // ⭐️ Zora
-        11155111: "rpc.sepolia.org", // Sepolia
-        245022926: "devnet.neonevm.org", // ⭐️ Neon EVM DevNet
-        245022934: "neon-proxy-mainnet.solana.p2p.org/", // ⭐️ Neon EVM MainNet
-        999999999: "sepolia.rpc.zora.energy", // Zora Sepolia Testnet
-        1313161554: "mainnet.aurora.dev", // ⭐️ Aurora Mainnet
-        1313161555: "testnet.aurora.dev/", // Aurora Testnet
-        1666600000: "api.harmony.one", // 🙄 Harmony Mainnet Shard 0
-        11297108099: "palm-testnet.infura.io/v3/", // Palm Testnet
-        11297108109: "palm-mainnet.infura.io/v3/", // Palm
+        1: "mainnet.infura.io/v3/",
+        5: "goerli.infura.io/v3/",
+        10: "optimism-mainnet.infura.io/v3/",
+        56: "bsc-dataseed1.bnbchain.org",
+        66: "exchainrpc.okex.org",
+        69: "kovan.optimism.io/",
+        77: "sokol.poa.network",
+        97: "data-seed-prebsc-1-s1.bnbchain.org:8545",
+        99: "core.poa.network",
+        100: "rpc.gnosischain.com",
+        122: "rpc.fuse.io",
+        128: "http-mainnet.hecochain.com",
+        137: "polygon-mainnet.infura.io/v3/",
+        200: "arbitrum.xdaichain.com/",
+        204: "opbnb-mainnet-rpc.bnbchain.org",
+        250: "rpc.ftm.tools",
+        280: "testnet.era.zksync.dev",
+        314: "api.node.glif.io/",
+        324: "mainnet.era.zksync.io",
+        420: "goerli.optimism.io",
+        424: "rpc.publicgoods.network",
+        1001: "api.baobab.klaytn.net:8651",
+        1088: "andromeda.metis.io/?owner=1088",
+        1101: "zkevm-rpc.com",
+        1442: "rpc.public.zkevm-test.net",
+        2221: "evm.testnet.kava.io",
+        2222: "evm.kava.io",
+        4002: "rpc.testnet.fantom.network",
+        4337: "subnets.avax.network/beam/mainnet/rpc",
+        5000: "rpc.mantle.xyz",
+        5001: "rpc.testnet.mantle.xyz",
+        5611: "opbnb-testnet-rpc.bnbchain.org",
+        7700: "canto.slingshot.finance",
+        8217: "1rpc.io/klay",
+        8453: "mainnet.base.org/",
+        10200: "rpc.chiadochain.net",
+        42161: "arbitrum-mainnet.infura.io/v3/",
+        42170: "nova.arbitrum.io/rpc",
+        42220: "forno.celo.org",
+        43113: "api.avax-test.network/ext/bc/C/rpc",
+        43114: "api.avax.network/ext/bc/C/rpc",
+        44787: "alfajores-forno.celo-testnet.org",
+        58008: "sepolia.publicgoods.network",
+        59140: "rpc.goerli.linea.build",
+        59144: "rpc.linea.build",
+        64240: "rpcapi.sonic.fantom.network/",
+        80001: "rpc-mumbai.maticvigil.com",
+        84531: "goerli.base.org",
+        84532: "sepolia.base.org",
+        421613: "goerli-rollup.arbitrum.io/rpc",
+        421614: "sepolia-rollup.arbitrum.io/rpc",
+        534351: "sepolia-rpc.scroll.io",
+        534352: "rpc.scroll.io",
+        7777777: "rpc.zora.energy/",
+        11155111: "rpc.sepolia.org",
+        245022926: "devnet.neonevm.org",
+        245022934: "neon-proxy-mainnet.solana.p2p.org/",
+        999999999: "sepolia.rpc.zora.energy",
+        1313161554: "mainnet.aurora.dev",
+        1313161555: "testnet.aurora.dev/",
+        1666600000: "api.harmony.one",
+        11297108099: "palm-testnet.infura.io/v3/",
+        11297108109: "palm-mainnet.infura.io/v3/"
     ]
     
 }
