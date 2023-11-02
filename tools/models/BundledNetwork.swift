d68d5d713a738b366bd6dc091d8f3e0b28b955d3
ivan grachev <ivan@ivan.lol>
2023-11-02 18:52:12 +0300

    update bundled network model

--- tools/models/BundledNetwork.swift
+++ tools/models/BundledNetwork.swift
@@ -3,6 +3,35 @@
 import Foundation
 
 struct BundledNetwork: Codable {
+    
     let name: String
     let symbol: String
+    let isTest: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        case name = "n"
+        case symbol = "s"
+        case isTest = "t"
+    }
+    
+    init(name: String, symbol: String, isTest: Bool) {
+        self.name = name
+        self.symbol = symbol
+        self.isTest = isTest
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        name = try container.decode(String.self, forKey: .name)
+        symbol = try container.decode(String.self, forKey: .symbol)
+        isTest = try container.decodeIfPresent(Bool.self, forKey: .isTest) ?? false
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(name, forKey: .name)
+        try container.encode(symbol, forKey: .symbol)
+        if isTest { try container.encode(isTest, forKey: .isTest) }
+    }
+    
 }
