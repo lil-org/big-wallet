74df1603d8fc7e21e4ab5d266fa7bce4b27ce01d
ivan grachev <ivan@ivan.lol>
2023-11-23 05:27:59 +0300

    update path

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,5 +3,5 @@ set -e
 
 if [ "$CI_PRODUCT_PLATFORM" = 'macOS' ] && [ "$CI_XCODEBUILD_ACTION" = 'build-for-testing' ]; then
     sed -i'~' 's/ENABLE_HARDENED_RUNTIME = YES;/ENABLE_HARDENED_RUNTIME = NO;/g' \
-        "$CI_PROJECT_FILE_PATH/project.pbxproj"
+        "$CI_PROJECT_FILE_PATH"
 fi
