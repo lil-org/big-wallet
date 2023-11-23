b3d5452ad7eefcd23f08717760ff7af64da232ac
ivan grachev <ivan@ivan.lol>
2023-11-24 02:04:13 +0300

    tune network selection on macos

--- Shared/Views/NetworksListView.swift
+++ Shared/Views/NetworksListView.swift
@@ -27,7 +27,14 @@ struct NetworksListView: View {
                 }.disabled(selectedNetwork == nil))
         }
 #elseif os(macOS)
-        list()
+        VStack {
+            list()
+            HStack {
+                Button(Strings.cancel) { presentationMode.wrappedValue.dismiss() }.keyboardShortcut(.cancelAction)
+                Button(Strings.ok) { completion(selectedNetwork); presentationMode.wrappedValue.dismiss() }.keyboardShortcut(.defaultAction)
+                    .disabled(selectedNetwork == nil)
+            }.frame(height: 36).offset(CGSize(width: 0, height: -6))
+        }
 #endif
     }
     
