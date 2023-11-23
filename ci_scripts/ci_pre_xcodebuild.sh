599c4dbec00a725b067136a8ddc5fa03ebfe5d7a
ivan grachev <ivan@ivan.lol>
2023-11-23 05:35:04 +0300

    use CI_PRIMARY_REPOSITORY_PATH

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -3,5 +3,5 @@ set -e
 
 if [ "$CI_PRODUCT_PLATFORM" = 'macOS' ] && [ "$CI_XCODEBUILD_ACTION" = 'build-for-testing' ]; then
     sed -i'~' 's/ENABLE_HARDENED_RUNTIME = YES;/ENABLE_HARDENED_RUNTIME = NO;/g' \
-        "$CI_PROJECT_FILE_PATH"
+        "$CI_PRIMARY_REPOSITORY_PATH/$CI_XCODE_PROJECT/project.pbxproj"
 fi
