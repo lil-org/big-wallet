89646cd0df9e8638ec932257fd573ec18f5a96f7
ivan grachev <ivan@ivan.lol>
2023-12-05 13:47:59 +0300

    clean up wallet model

--- Shared/Wallets/TokenaryWallet.swift
+++ Shared/Wallets/TokenaryWallet.swift
@@ -22,17 +22,9 @@ final class TokenaryWallet: Hashable, Equatable {
         self.key = key
     }
     
-    // TODO: remake since there will be several accounts for a single coin now
     func getAccount(password: String, coin: CoinType) throws -> Account {
         let wallet = key.wallet(password: Data(password.utf8))
         guard let account = key.accountForCoin(coin: coin, wallet: wallet) else { throw KeyStore.Error.invalidPassword }
-        return account
-    }
-    
-    // TODO: remake since there will be several accounts for a single coin now. do not use derivation model, it is no good
-    func getAccount(password: String, coin: CoinType, derivation: Derivation) throws -> Account {
-        let wallet = key.wallet(password: Data(password.utf8))
-        guard let account = key.accountForCoinDerivation(coin: coin, derivation: derivation, wallet: wallet) else { throw KeyStore.Error.invalidPassword }
         return account
     }
 
