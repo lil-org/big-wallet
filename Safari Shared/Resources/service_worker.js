943c7c422063c01c58e6b48842361ffc7b9f388f
ivan grachev <ivan@ivan.lol>
2023-12-21 21:38:49 +0300

    pass rpc requests through service worker

--- Safari Shared/Resources/service_worker.js
+++ Safari Shared/Resources/service_worker.js
@@ -1,7 +1,13 @@
 // Copyright © 2023 Tokenary. All rights reserved.
 
 function handleOnMessage(request, sender, sendResponse) {
-    if (request.subject === "message-to-wallet") {
+    if (request.subject === "rpc") {
+        browser.runtime.sendNativeMessage("mac.tokenary.io", request).then(response => {
+            if (typeof response !== "undefined") {
+                sendResponse(response);
+            } else { sendResponse(); }
+        }).catch(() => { sendResponse(); });
+    } else if (request.subject === "message-to-wallet") {
         browser.runtime.sendNativeMessage("mac.tokenary.io", request.message).then(response => {
             if (typeof response !== "undefined") {
                 sendResponse(response);
