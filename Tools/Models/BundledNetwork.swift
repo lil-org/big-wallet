081ac914b00f26d9faf81b41732a876dfd327952
ivan grachev <ivan@ivan.lol>
2024-02-08 01:15:25 +0300

    update bundled network model

--- Tools/Models/BundledNetwork.swift
+++ Tools/Models/BundledNetwork.swift
@@ -8,25 +8,29 @@ struct BundledNetwork: Codable {
     let symbol: String
     let isTest: Bool
     let okToShowPriceForSymbol: Bool
+    let blockExplorer: String
     
     private enum CodingKeys: String, CodingKey {
         case name = "n"
         case symbol = "s"
         case isTest = "t"
         case okToShowPriceForSymbol = "o"
+        case blockExplorer = "b"
     }
     
-    init(name: String, symbol: String, isTest: Bool, okToShowPriceForSymbol: Bool) {
+    init(name: String, symbol: String, isTest: Bool, okToShowPriceForSymbol: Bool, blockExplorer: String) {
         self.name = name
         self.symbol = symbol
         self.isTest = isTest
         self.okToShowPriceForSymbol = okToShowPriceForSymbol
+        self.blockExplorer = blockExplorer
     }
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         name = try container.decode(String.self, forKey: .name)
         symbol = try container.decode(String.self, forKey: .symbol)
+        blockExplorer = try container.decode(String.self, forKey: .blockExplorer)
         isTest = try container.decodeIfPresent(Bool.self, forKey: .isTest) ?? false
         if isTest {
             okToShowPriceForSymbol = false
@@ -39,6 +43,7 @@ struct BundledNetwork: Codable {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(name, forKey: .name)
         try container.encode(symbol, forKey: .symbol)
+        try container.encode(blockExplorer, forKey: .blockExplorer)
         if isTest { try container.encode(isTest, forKey: .isTest) }
         if okToShowPriceForSymbol { try container.encode(okToShowPriceForSymbol, forKey: .okToShowPriceForSymbol) }
     }
