0b0a6785930072add8fb4a0a411c349aaf2cfb60
ivan grachev <ivan@ivan.lol>
2025-01-03 12:42:23 +0300

    fix translator

--- translate/main.swift
+++ translate/main.swift
@@ -7,6 +7,6 @@ let queue = DispatchQueue(label: UUID().uuidString, qos: .default)
 let projectDir = FileManager.default.currentDirectoryPath
 
 translateAppStoreMetadata(.highQuality)
-//translateAllStrings(.highQuality)
+translateAllStrings(.highQuality)
 
 print("🟢 all done")
