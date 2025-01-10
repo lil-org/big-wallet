18b603257f92339910026f9b6ae2c8d62c71b653
ivan grachev <ivan@ivan.lol>
2025-01-10 12:47:19 +0300

    update review requester

--- Shared/Services/ReviewRequester.swift
+++ Shared/Services/ReviewRequester.swift
@@ -4,25 +4,29 @@ import StoreKit
 
 struct ReviewRequster {
     
-    static func requestReviewIfNeeded() {
+    static func requestReviewIfNeeded(in scene: Any?) {
         if let lastDate = Defaults.latestReviewRequestDate {
             if Date().timeIntervalSince(lastDate) > 190 * 24 * 3600 {
-                requestReview()
+                requestReview(in: scene)
             }
         } else {
             if Defaults.reviewRequestsGoodMomentsCount >= 2 {
-                requestReview()
+                requestReview(in: scene)
             } else {
                 Defaults.reviewRequestsGoodMomentsCount += 1
             }
         }
     }
     
-    private static func requestReview() {
-        #if os(macOS)
-            SKStoreReviewController.requestReview()
-            Defaults.latestReviewRequestDate = Date()
-        #endif
+    private static func requestReview(in scene: Any?) {
+#if os(macOS)
+        SKStoreReviewController.requestReview()
+#else
+        if let scene = scene as? UIWindowScene {
+            SKStoreReviewController.requestReview(in: scene)
+        }
+#endif
+        Defaults.latestReviewRequestDate = Date()
     }
     
 }
