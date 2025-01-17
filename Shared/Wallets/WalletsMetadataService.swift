89dfd7c4724820017b15249905dbcfbf36270aa4
ivan grachev <ivan@ivan.lol>
2025-01-17 14:24:00 +0300

    stub wallets metadata service

---
+++ Shared/Wallets/WalletsMetadataService.swift
@@ -0,0 +1,17 @@
+// ∅ 2025 lil org
+
+import WalletCore
+
+struct WalletsMetadataService {
+    
+    private init() {}
+    
+    static func getName(wallet: WalletContainer, account: Account) -> String {
+        return "" // TODO: implement
+    }
+    
+    static func saveName(_ name: String, wallet: WalletContainer, account: Account) {
+        // TODO: implement
+    }
+    
+}
