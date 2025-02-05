36ea045519db65e37dbcd85af88e379e734cecc1
ivan grachev <ivan@ivan.lol>
2025-02-05 20:52:41 +0300

    show password screen on visionos, add run script build phases for visionos

--- App visionOS/Big_Wallet_visionOSApp.swift
+++ App visionOS/Big_Wallet_visionOSApp.swift
@@ -9,8 +9,8 @@ var launchURL: URL?
 struct Big_Wallet_visionOSApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
-        }
+            ViewControllerWrapper()
+        }.defaultSize(CGSize(width: 350, height: 500))
     }
 }
 
@@ -19,4 +19,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
+}
+
+struct ViewControllerWrapper: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        return createInitialViewController()
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+
+    }
+}
+
+func createInitialViewController() -> UIViewController {
+    let vc = instantiate(PasswordViewController.self, from: .main)
+    return vc.inNavigationController
 }
