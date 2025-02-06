e3c57aa88fad1839524d525a9fe63e11601c59b3
ivan grachev <ivan@ivan.lol>
2025-02-06 20:39:06 +0300

    handle incoming urls on visionos

--- App visionOS/Big_Wallet_visionOSApp.swift
+++ App visionOS/Big_Wallet_visionOSApp.swift
@@ -5,26 +5,62 @@ import SwiftUI
 let screenshotMode = false
 var launchURL: URL?
 
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        launchURL = url
+        didReceiveWalletRequest()
+        return true
+    }
+    
+    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        if let url = userActivity.webpageURL {
+            launchURL = url
+            didReceiveWalletRequest()
+            return true
+        }
+        return false
+    }
+    
+}
+
 @main
 struct Big_Wallet_visionOSApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ViewControllerWrapper()
-        }.defaultSize(CGSize(width: 420, height: 555))
+                .onOpenURL { url in
+                    launchURL = url
+                    didReceiveWalletRequest()
+                }
+        }
+        .defaultSize(CGSize(width: 420, height: 555))
     }
+    
 }
 
 struct ViewControllerWrapper: UIViewControllerRepresentable {
+    
     func makeUIViewController(context: Context) -> UIViewController {
         return createInitialViewController()
     }
     
     func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-
+        
     }
+    
 }
 
 func createInitialViewController() -> UIViewController {
     let vc = instantiate(PasswordViewController.self, from: .main)
     return vc.inNavigationController
+}
+
+func didReceiveWalletRequest() {
+    // TODO: implement. i.e. ios version sends notif here
+    // NotificationCenter.default.post(name: .receievedWalletRequest, object: nil)
+    launchURL = nil
 }
