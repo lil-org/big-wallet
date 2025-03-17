67372f1f35f04942a918d10b2a2e3b90e12e4104
ivan grachev <ivan@ivan.lol>
2025-03-17 16:55:07 +0300

    update bundled networks

--- Tools/main.swift
+++ Tools/main.swift
@@ -30,7 +30,7 @@ fetchChains { chains in
     let currentNodes = try! JSONDecoder().decode([String: String].self, from: currentNodesData)
     
     let currentIds = Set(currentNetworks.keys)
-    let newChainsIds = Set([146])
+    let newChainsIds = Set([33139, 8333, 80094, 747, 1329, 360, 1868, 130])
     
     let newChains = chains.filter { chain in
         let isEIP3091 = chain.explorers?.contains(where: { $0.standard == "EIP3091" }) == true
