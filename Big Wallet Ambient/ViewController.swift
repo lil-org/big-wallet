a8325e039aebaf42b56abef4768ceee40ce7ebe0
ivan grachev <ivan@ivan.lol>
2026-04-29 17:45:30 +0300

    stub separate ambient agent target

---
+++ Big Wallet Ambient/ViewController.swift
@@ -0,0 +1,21 @@
+// ∅ 2026 lil org
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
