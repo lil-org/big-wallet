bcd5e4f48986bac943572631b6dbe87792e2d654
ivan grachev <ivan@ivan.lol>
2026-07-10 20:19:18 +0300

    new chains

--- Tools/main.swift
+++ Tools/main.swift
@@ -30,7 +30,7 @@ fetchChains { chains in
     let currentNodes = try! JSONDecoder().decode([String: String].self, from: currentNodesData)
     
     let currentIds = Set(currentNetworks.keys)
-    let newChainsIds = Set([30, 10143])
+    let newChainsIds = Set([4326, 4217, 196, 1868, 130, 4663])
     
     let newChains = chains.filter { chain in
         let isEIP3091 = chain.explorers?.contains(where: { $0.standard == "EIP3091" }) == true
